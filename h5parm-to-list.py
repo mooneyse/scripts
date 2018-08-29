3e351f05668183a38d03827c5672a9dcd9a17bad
mooneyse <mooneyse@tcd.ie>
2018-08-29 14:13:26 +0200

    file name change

--- h5parm-to-list.py
+++ h5parm-to-list.py
@@ -6,11 +6,11 @@ def h5_values(my_h5parm, station):
     lo = lh5.h5parm(my_h5parm, readonly = False)
     phase = lo.getSolset('sol000').getSoltab('phase000')
     values = phase.val[0, 0, station, 0, :] # (pol, dir, ant, freq, time)
-           
+
     lo.close()
 
     return values
-    
+
 values = h5_values('../other/test.h5', 0)
 
 print(values)
